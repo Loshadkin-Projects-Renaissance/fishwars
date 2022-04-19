cd5dda80c0c53155e9ea9966353c351085818feb
vezono <vezonolab@gmail.com>
2022-04-19 19:09:16 +0300

    mongo debug

--- db.py
+++ db.py
@@ -171,7 +171,7 @@ class Database:
 
     def global_strength_regen(self, global_time):
         users = self.users.find({"$expr": {'$gt': ['$maxstrenght', '$strenght']}})
-        print(users.size())
+        print(len(users))
         for user in users:
             if not user['laststrenghtregen']:
                 self.regen_strength(user)
